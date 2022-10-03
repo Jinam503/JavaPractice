0ffe0540231b2a7ca0d43b5e6004e95a53fc0846
Jinam503 <jinam1234567@gmail.com>
2022-10-04 00:08:19 +0900

    Update 2.java

--- 2.java
+++ 2.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 
 class Main2 {
+    static int count;
+    static String a;
     public static int recursion(String s, int l, int r){
+        count++;
         if(l >= r) return 1;
         else if(s.charAt(l) != s.charAt(r)) return 0;
         else return recursion(s, l+1, r-1);
@@ -12,9 +15,12 @@ class Main2 {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-        
-        
-        System.out.println("ABBA: " + isPalindrome("ABBA"));
-        System.out.println("ABC: " + isPalindrome("ABC"));
+        a = in.nextLine();
+        for(int i = 0; i < N; i++){
+            a = in.nextLine();
+            count = 0;
+            System.out.println(isPalindrome(a)+ " " + count);
+        }
+        in.close();
     }
 }
